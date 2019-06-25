5bddbc055c91d37e14e71ca5e27694aed48daf2c
temoncher <temoncher@yandex.ru>
2019-06-25 23:54:58 +0300

    Namespaces cleanup (2)

--- Components/CategoryMenu.cs
+++ Components/CategoryMenu.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using TabletopStore.Services;
+using TabletopStore.Data.Services;
 
 namespace TabletopStore.Components
 {
